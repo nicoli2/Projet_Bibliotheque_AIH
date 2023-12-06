22141eb0347928bfe5143664e18e2592d9450444
Mohssine15 <146738652+Mohssine15@users.noreply.github.com>
2023-12-06 10:36:48 -0500

    Update main.py
    Modification de menu - main

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-#menu et programme
-menu ="""*************************************************
+#menu et programme principale qui gére la bibliothéque et fait appel aux classes: Adherent, Livre et Emprunt
+from Adherent import Adherent
+from Livre import Livre
+from Emprunt import Emprunt
+menu ="""
+*************************************************
 *      Bienvenue à votre bibliothèque           *
 *              Faites un choix :                *
 *************************************************
 *    1       Ajouter un adhérent                *
 *    2       Supprimer un adhérent              *
-*    3       Afficher tous les adhérents        * 
+*    3       Afficher tous les adhérents        *
 *    4       Ajouter un livre                   *
 *    5       Supprimer un livre                 *
 *    6       Afficher tous les livre            *
@@ -17,52 +21,95 @@ menu ="""*************************************************
  """
 while True:
     print(menu)
+    print("""
+===================================================
+================ Menu principal ===================
+===================================================
+    """)
     choix = input("Saisir votre choix selon le menu : ")
     if choix == "1":
-        print("============ Ajout d'adhérent =============")
-        from Adherent import Adherent
+        print("""
+===================================================
+================ Ajout d'adhérent =================
+===================================================
+        """)
         ad1 = Adherent()
         ad1.lireDepuisClavier()
         ad1.enregistrerDansFichier()
-        elif choix == "2":
-        print("============ Suppression d'adhérent =============")
-        from Adherent import Adherent
+    elif choix == "2":
+        print("""
+===================================================
+============ Suppression d'adhérent ===============
+===================================================
+        """)
         ad1 = Adherent()
         ad1.supprimerAdherent()
-        elif choix == "3":
-        print("============= La liste des adhérents ============")
-        from Adherent import Adherent
+    elif choix == "3":
+        print("""
+===================================================
+============== La liste des adhérents =============
+===================================================
+        """)
         ad1 = Adherent()
         ad1.afficherListeAdherent()
-        print("=================================================")
-        elif choix == "4":
-        print("================ Ajout de livre =================")
-        from Livre import Livre
+    elif choix == "4":
+        print("""
+===================================================
+=============== Ajout de livre ====================
+===================================================
+        """)
         liv1 = Livre()
         liv1.lireDepuisClavier()
         liv1.enregistrerDansFichier()
-        elif choix == "5":
-        print("============= Suppression de livre ==============")
-        from Livre import Livre
+    elif choix == "5":
+        print("""
+===================================================
+============ Suppression de livre =================
+===================================================
+        """)
         liv1 = Livre()
         liv1.supprimerLivre()
-        elif choix == "6":
-        print("=============== La liste des livres =============")
-        from Livre import Livre
+    elif choix == "6":
+        print("""
+===================================================
+================ La liste des livres ==============
+===================================================
+        """)
         liv1 = Livre()
         liv1.afficherListeLivre()
-        print("=================================================")
-        elif choix == "7":
-        print("Ajouter Emprunts")
-        print("Appeler un methode respective")
-        elif choix == "8":
-        print("Retour d’un  Emprunts")
-        print("Appeler un methode respective")
-        elif choix == "9":
-        print("Afficher tous les Emprunts")
-        print("Appeler un methode respective")
-        elif choix == "Q" or choix == "q":
-        print("Quitter, Merci d'avoire utiliser la bibliothèque ")
+    elif choix == "7":
+        print("""
+===================================================
+=============== Ajout d'emprunt ===================
+===================================================
+        """)
+        em1 = Emprunt()
+        em1.lireDepuisClavier()
+        em1.enregistrerDansFichier()
+    elif choix == "8":
+        print("""
+===================================================
+================ Retour d'un livre ================
+===================================================
+        """)
+        em1 = Emprunt()
+        em1.supprimerEmprunt()
+    elif choix == "9":
+        print("""p
+===================================================
+============== La liste des emprunts  =============
+===================================================
+        """)
+        em1 = Emprunt()
+        em1.afficherListeEmprunt()
+    elif choix == "Q" or choix == "q":
+        print("""
+===================================================
+======== Vous avez quitté l'application ! =========
+============ Merci d'avoire utiliser ==============
+============== notre bibliothèque =================
+===================================================
+        """)
         break
     else:
         print("!!! Choix erroné! Saisir à nouveaux !!!  ")
