1f83bba0adae0ef098487c503682caa1d96ff0ab
HasBen1970 <150358615+HasBen1970@users.noreply.github.com>
2023-12-06 10:51:58 -0500

    Update Adherent.py
    Modification class Adherent

--- Adherent.py
+++ Adherent.py
@@ -1,3 +1,4 @@
+# Classe Adhérent permet d'ajouter un adhérent, d'afficher la liste des adhérents et supprimer un adhérent
 class Adherent:
     def __init__(self, p_nom="", p_prenom="", p_num=""):
         self.nom = p_nom
@@ -10,19 +11,27 @@ class Adherent:
             if n.isnumeric():
                 x = 0
                 while x < int(n):
-                    print("===============================================")
+                    print("""
+    ===================================================
+                    """)
                     self.nom = input("Nom d'adhérent: ")
                     self.prenom = input("Prénom d'adhérent: ")
                     self.num = input("Num d'adhérent: ")
                     d = str(self.nom) + ", " + str(self.prenom) + ", " + str(self.num)
                     listeAdherent.append(d)
                     x = x + 1
-                print("========== Liste des adhérents ajoutés: ============")
+                print("""
+    ===================================================
+    ========= Liste des adhérents ajoutés  ============
+    ===================================================
+                """)
                 x = 0
                 while x < int(n):
                     print("Adhérent num: ", x + 1, "\n", listeAdherent[x])
                     x = x + 1
-                print("====================================================")
+                print("""
+    ===================================================
+                """)
                 flag=0
             else:
                 print("Saisie non accptée !!! Saisir un nombre ")
@@ -48,10 +57,17 @@ class Adherent:
             x = input("Saisir l'ID d'adhérent à supprimer: ")
             if x.isnumeric() and int(x)-1 < len(ligne):
                 flag = 0
-                print("====== ATTENTION !!! Cet adhérent sera supprimé !!! =========")
+                print("""
+    ===================================================
+    ================= ATTENTION !!!  ==================
+    ========== Cet adhérent sera supprimé !!!==========
+    ===================================================
+                """)
                 print("\n", ligne[int(x)-1])
-                print("===================================================================")
-                y = input ("Êtes-vous sûr de le supprimer ? O/N ")
+                print("""
+    ===================================================
+                """)
+                y = input ("Êtes-vous sûr de vouloir le supprimer ? O/N ")
                 if y == "o" or y == "O":
                     listeTemp = []
                     for i in range(0, len(ligne)):
@@ -61,21 +77,23 @@ class Adherent:
                     for x in listeTemp:
                         f.write(x)
                     f.close()
-                    print("========= Suppression d'adhérent réussi =========")
+                    print("""
+    ===================================================
+    ========= Suppression d'adhérent réussie ==========
+    ===================================================
+                    """)
                 else:
-                    print("============== Suppression d'adhérent annulée =================")
+                    print("""
+    ====================================================
+    ========= Suppression d'adhérent annulée ===========
+    ====================================================
+                    """)
                 flag = 0
             else:
-                print("Ce numéro d'adhérent n'existe pas !!!")
+                print("""
+    ===================================================
+    ======= Ce numéro d'adhérent n'existe pas !!! =====
+    ===================================================
+                """)
                 flag = 1
 listeAdherent = []
-#Création d'objets
-# ad1 = Adherent()
-# ad1.lireDepuisClavier()
-# ad1.enregistrerDansFichier()
-# print("=====La liste des adérents=============")
-# ad1.afficherListeAdherent()
-# ad1.supprimerAdherent()
-
-
-
