8c44ed73af9c546e14b10ba79959778c42ffd8db
nicoli2 <146738691+nicoli2@users.noreply.github.com>
2023-12-06 10:28:33 -0500

    Update main.py
    True

--- main.py
+++ main.py
@@ -15,9 +15,8 @@ menu ="""*************************************************
 *    Q       Quitter                            *
 *************************************************
  """
-print(menu)
-flag = 1
-while flag == 1:
+while True:
+    print(menu)
     choix = input("Saisir votre choix selon le menu : ")
     if choix == "1":
         print("============ Ajout d'adhérent =============")
@@ -25,55 +24,45 @@ while flag == 1:
         ad1 = Adherent()
         ad1.lireDepuisClavier()
         ad1.enregistrerDansFichier()
-        flag = 1
-    elif choix == "2":
+        elif choix == "2":
         print("============ Suppression d'adhérent =============")
         from Adherent import Adherent
         ad1 = Adherent()
         ad1.supprimerAdherent()
-        flag = 1
-    elif choix == "3":
+        elif choix == "3":
         print("============= La liste des adhérents ============")
         from Adherent import Adherent
         ad1 = Adherent()
         ad1.afficherListeAdherent()
         print("=================================================")
-        flag = 1
-    elif choix == "4":
+        elif choix == "4":
         print("================ Ajout de livre =================")
         from Livre import Livre
         liv1 = Livre()
         liv1.lireDepuisClavier()
         liv1.enregistrerDansFichier()
-        flag = 1
-    elif choix == "5":
+        elif choix == "5":
         print("============= Suppression de livre ==============")
         from Livre import Livre
         liv1 = Livre()
         liv1.supprimerLivre()
-        flag = 1
-    elif choix == "6":
+        elif choix == "6":
         print("=============== La liste des livres =============")
         from Livre import Livre
         liv1 = Livre()
         liv1.afficherListeLivre()
         print("=================================================")
-        flag = 1
-    elif choix == "7":
+        elif choix == "7":
         print("Ajouter Emprunts")
         print("Appeler un methode respective")
-        flag = 1
-    elif choix == "8":
+        elif choix == "8":
         print("Retour d’un  Emprunts")
         print("Appeler un methode respective")
-        flag = 1
-    elif choix == "9":
+        elif choix == "9":
         print("Afficher tous les Emprunts")
         print("Appeler un methode respective")
-        flag = 1
-    elif choix == "Q" or choix == "q":
+        elif choix == "Q" or choix == "q":
         print("Quitter, Merci d'avoire utiliser la bibliothèque ")
         break
     else:
         print("!!! Choix erroné! Saisir à nouveaux !!!  ")
-        flag = 1
